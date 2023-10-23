8a708e38ce4254dbae1064b8d29c0c420b1423bc
saif64s <my@wo.rk>
2023-10-23 17:59:14 +0600

    complete app

---
+++ README.md
@@ -0,0 +1,22 @@
+# news_rest_api
+
+## Features
+
+- Feteches data from API
+- has dark and light mode
+- search based on topic
+- refreshes the news
+- Filter with category and time
+- Natvie webView and share
+- Open in your browser
+- Add to Bookmark
+
+<p float="left">
+  <img src="https://github.com/Saif64s/flutter-restAPi_news/blob/main/assets/screenshots/Screenshot%202023-07-09%20at%2012.00.00.png" width="250" />
+  <img src="https://github.com/Saif64s/flutter-restAPi_news/blob/main/assets/screenshots/Screenshot%202023-07-09%20at%2012.00.10.png" width="250" /> 
+  <img src="https://github.com/Saif64s/flutter-restAPi_news/blob/main/assets/screenshots/Screenshot%202023-07-09%20at%2012.00.21.png" width="250" />
+  <img src="https://github.com/Saif64s/flutter-restAPi_news/blob/main/assets/screenshots/Screenshot%202023-07-09%20at%2012.01.04.png" width="250" />
+  <img src="https://github.com/Saif64s/flutter-restAPi_news/blob/main/assets/screenshots/Screenshot%202023-07-09%20at%2012.01.11.png" width="250" />
+  <img src="https://github.com/Saif64s/flutter-restAPi_news/blob/main/assets/screenshots/Screenshot%202023-07-09%20at%2012.01.18.png" width="250" />
+  <img src="https://github.com/Saif64s/flutter-restAPi_news/blob/main/assets/screenshots/Screenshot%202023-07-09%20at%2012.01.28.png" width="250" />
+</p>
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+//Packages
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:news_rest_api/screens/blog_details.dart';
+import 'package:news_rest_api/screens/home_screen.dart';
+import 'package:provider/provider.dart';
+
+import 'consts/theme_data.dart';
+
+import 'providers/theme_provider.dart';
+
+void main() async {
+  runApp(const NewsRestAPI());
+  await dotenv.load(fileName: ".env");
+}
+
+class NewsRestAPI extends StatefulWidget {
+  const NewsRestAPI({Key? key}) : super(key: key);
+
+  @override
+  State<NewsRestAPI> createState() => _NewsRestAPIState();
+}
+
+class _NewsRestAPIState extends State<NewsRestAPI> {
+  //Need it to access the theme Provider
+  ThemeProvider themeChangeProvider = ThemeProvider();
+
+  @override
+  void initState() {
+    getCurrentAppTheme();
+    super.initState();
+  }
+
+  //Fetch the current theme
+  void getCurrentAppTheme() async {
+    themeChangeProvider.setDarkTheme =
+        await themeChangeProvider.darkThemePreferences.getTheme();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) {
+          //Notify about theme changes
+          return themeChangeProvider;
+        }),
+      ],
+      child:
+          //Notify about theme changes
+          Consumer<ThemeProvider>(builder: (context, themeChangeProvider, ch) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'News App',
+          theme: Styles.themeData(themeChangeProvider.getDarkTheme, context),
+          home: const HomeScreen(),
+          routes: {
+            NewsDetailsScreen.routeName: (ctx) => const NewsDetailsScreen()
+          },
+        );
+      }),
+    );
+  }
+}
